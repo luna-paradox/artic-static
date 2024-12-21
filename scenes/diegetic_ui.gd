9a43aa11335ef14c2ada2eab802e36383e750167
luna-paradox <luna_paradox@proton.me>
2024-12-21 19:58:47 -0300

    diegetic indicator for energy

--- scenes/diegetic_ui.gd
+++ scenes/diegetic_ui.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 @onready var player = $".."
+@onready var main = $"../.."
 @onready var energy_light = $energy_light
+@onready var energy_light_point = $energy_light/light
 @onready var turbo_light = $turbo_light
 
 func _process(_delta: float) -> void:
@@ -9,6 +11,9 @@ func _process(_delta: float) -> void:
 		update_turbo_light_visibility(true)
 	else:
 		update_turbo_light_visibility(false)
+	
+	var energy_percentage: float = float(main.current_energy) / main.MAX_ENERGY
+	update_energy_light_alpha(energy_percentage)
 
 
 func update_turbo_light_visibility(new_visibility) -> void:
@@ -16,3 +21,7 @@ func update_turbo_light_visibility(new_visibility) -> void:
 		return
 	
 	turbo_light.visible = new_visibility
+
+func update_energy_light_alpha(alpha_percentage: float) -> void:
+	energy_light.modulate.a = alpha_percentage
+	energy_light_point.energy = alpha_percentage * 16
