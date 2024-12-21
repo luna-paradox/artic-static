e25b9c79cfb87dc2787ce6e6569855861f2a0581
luna-paradox <luna_paradox@proton.me>
2024-12-21 16:27:07 -0300

    more refactor

--- scenes/player.gd
+++ scenes/player.gd
@@ -13,12 +13,10 @@ var pause: bool = true
 @export var DECELERATION: int = 100
 @export var MAX_SPEED: int = 400
 
-
-var sonar_timer: Timer
 @onready var virtual_position: Node2D = $"../virtual_player_pos"
-@onready var sonar: Node2D = $"../virtual_player_pos/sonar"
-@onready var sonar_sprite: Node2D = $"../virtual_player_pos/sonar/sprite"
 
+
+# ---- BUILT-IN ----
 func _ready() -> void:
 	sonar_timer = Timer.new()
 	sonar_timer.wait_time = 2
@@ -26,31 +24,14 @@ func _ready() -> void:
 	sonar_timer.timeout.connect(hide_sonnar)
 	add_child(sonar_timer)
 
-var is_sonar_enabled = false
-var target_node: Node2D
-
-func activate_sonar(new_target_node: Node2D) -> void:
-	is_sonar_enabled = true
-	target_node = new_target_node
-	
-	if !sonar.visible:
-		sonar.show()
-		sonar_timer.start()
-
-func hide_sonnar() -> void:
-	sonar.hide()
-	is_sonar_enabled = false
-
-
 func _process(_delta: float) -> void:
 	if is_sonar_enabled:
-		var new_angle = virtual_position.get_angle_to(target_node.global_position)
+		var new_angle = virtual_position.get_angle_to(sonar_target_node.global_position)
 		new_angle += deg_to_rad(90)
 		sonar.rotation = new_angle
 		
 		var new_alpha = sonar_timer.time_left / sonar_timer.wait_time
 		sonar_sprite.self_modulate.a = new_alpha
-
 
 func _input(event: InputEvent) -> void:
 	if pause:
@@ -59,11 +40,22 @@ func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("turn_sub"):
 		self.scale.x *= -1
 
-
 func _physics_process(delta):
 	if pause:
 		return
 	
+	# MOVE AND COLLIDE
+	var collision = move_vessel(delta)
+	crash_vessel(collision)
+
+func init(new_acceleration: int, new_deceleration: int, new_max_peed: int):
+	ACCELERATION = new_acceleration
+	DECELERATION = new_deceleration
+	MAX_SPEED = new_max_peed
+
+
+# ---- MOVEMENT ----
+func move_vessel(delta: float) -> KinematicCollision2D:
 	# GET INPUT
 	var direction = Input.get_vector("left", "right", "top", "bottom")
 	
@@ -83,30 +75,51 @@ func _physics_process(delta):
 	
 	# DETECT COLLISION
 	var collision = move_and_collide(velocity * delta)
-	if collision:
-		#BOUNCE
-		velocity = velocity.bounce(collision.get_normal()) / 3
-		
-		#DAMAGE
-		#print(str(velocity.length()))
-		var damage = floor(velocity.length() * 0.4)
-		main_controller.update_hp(-damage)
-		
-		# SFX
-		if !damage_sound.playing:
-			var damage_volume = 1
-			if velocity.length() < 100:
-				damage_volume = velocity.length() / 100
-			damage_sound.volume_db = linear_to_db(damage_volume)
-			damage_sound.play()
+	
+	return collision
+
+func crash_vessel(collision: KinematicCollision2D) -> void:
+	if !collision:
+		return
+	
+	#BOUNCE
+	velocity = velocity.bounce(collision.get_normal()) / 3
+	
+	#DAMAGE
+	#print(str(velocity.length()))
+	var damage = floor(velocity.length() * 0.4)
+	main_controller.update_hp(-damage)
+	
+	# SFX
+	if !damage_sound.playing:
+		var damage_volume = 1
+		if velocity.length() < 100:
+			damage_volume = velocity.length() / 100
+		damage_sound.volume_db = linear_to_db(damage_volume)
+		damage_sound.play()
 
 
-func init(new_acceleration: int, new_deceleration: int, new_max_peed: int):
-	ACCELERATION = new_acceleration
-	DECELERATION = new_deceleration
-	MAX_SPEED = new_max_peed
+# ---- SONNAR ----
+@onready var sonar: Node2D = $"../virtual_player_pos/sonar"
+@onready var sonar_sprite: Node2D = $"../virtual_player_pos/sonar/sprite"
+var sonar_target_node: Node2D
+var is_sonar_enabled = false
+var sonar_timer: Timer
+
+func activate_sonar(new_target_node: Node2D) -> void:
+	is_sonar_enabled = true
+	sonar_target_node = new_target_node
+	
+	if !sonar.visible:
+		sonar.show()
+		sonar_timer.start()
+
+func hide_sonnar() -> void:
+	sonar.hide()
+	is_sonar_enabled = false
 
 
+# ---- PAUSE ----
 func update_pause(new_state: bool) -> void:
 	pause = new_state
 	if pause:
