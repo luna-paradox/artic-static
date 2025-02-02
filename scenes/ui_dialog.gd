f1763b69a61bc5933330c3e6f73cd567e65d5162
luna-paradox <luna_paradox@proton.me>
2025-02-02 12:10:50 -0300

    faster dialog when pressing the progress dialog button

--- scenes/ui_dialog.gd
+++ scenes/ui_dialog.gd
@@ -18,9 +18,17 @@ var dialog_db = {
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("1_debug"):
-		load_next_line()
+		print(fast_dialog)
 	elif event.is_action_pressed("2_debug"):
 		load_dialog(dialog_db.test_dialog_0.route)
+	
+	if event.is_action_pressed("progress_dialog"):
+		if current_state == states.WAITING:
+			load_next_line()
+		fast_dialog = true
+		
+	if event.is_action_released("progress_dialog"):
+		fast_dialog = false
 
 
 # ------ CONTROLLER FUNCTIONS --------
@@ -44,12 +52,15 @@ func load_dialog(route: String) -> void:
 			"new_dialog_string": text,
 		})
 	
-	update_state(states.TALKING)
+	update_state(states.WAITING)
 	self.show()
 	load_next_line()
 
+var fast_dialog = false
+var wait_time = 0.02
+
 func load_next_line() -> void:
-	if current_state != states.TALKING:
+	if current_state != states.WAITING:
 		print('NO MORE DIALOG')
 		return
 	
@@ -57,7 +68,7 @@ func load_next_line() -> void:
 		update_state(states.INNACTIVE)
 		return
 
-	update_state(states.PAUSE)
+	update_state(states.TALKING)
 	
 	var current_line = all_dialog[current_dialog_index]
 	
@@ -65,8 +76,11 @@ func load_next_line() -> void:
 	
 	for character in current_line.new_dialog_string:
 		main_dialog_box.text = main_dialog_box.text + color_wrap_text(character, current_line.speaker_id)
-		var wait_time = 0.02
-		if character == '.':
+		
+		wait_time = 0.02
+		if fast_dialog:
+			wait_time = 0.001
+		elif character == '.':
 			wait_time = 0.15
 		
 		await get_tree().create_timer(wait_time).timeout
@@ -74,7 +88,7 @@ func load_next_line() -> void:
 	main_dialog_box.text = main_dialog_box.text + '\n'
 	
 	current_dialog_index += 1
-	update_state(states.TALKING)
+	update_state(states.WAITING)
 
 func reset_dialog_box() -> void:
 	all_dialog = []
@@ -84,7 +98,7 @@ func reset_dialog_box() -> void:
 
 
 # ------ STATE MACHINE --------
-enum states { PAUSE, INNACTIVE, TALKING }
+enum states { TALKING, INNACTIVE, WAITING }
 var current_state = states.INNACTIVE
 
 func update_state(new_state: states) -> void:
