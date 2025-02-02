250e4137542cb7ba81ce6797ad06857a3170ba0e
luna-paradox <luna_paradox@proton.me>
2025-02-02 00:57:26 -0300

    improve on dialog system

--- scenes/ui_dialog.gd
+++ scenes/ui_dialog.gd
@@ -8,36 +8,43 @@ var all_dialog = []
 var current_dialog_index = 0
 
 var dialog_db = {
-	"test_dialog": {
+	"test_dialog_0": {
 		"route": "res://dialogs/test.csv"
-	}
+	},
+	"test_dialog_1": {
+		"route": "res://dialogs/test_2.csv"
+	},
 }
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("1_debug"):
 		load_next_line()
 	elif event.is_action_pressed("2_debug"):
-		load_dialog(dialog_db.test_dialog.route)
+		load_dialog(dialog_db.test_dialog_0.route)
 
 
 # ------ CONTROLLER FUNCTIONS --------
-
 func load_dialog(route: String) -> void:
+	if current_state != states.INNACTIVE:
+		return
+	
 	reset_dialog_box()
 	var data = load(route)
 	
 	for line in data.records:
-		var text = line[line.keys()[3]]
 		var speaker_id = line[line.keys()[0]]
+		var text = line[line.keys()[4]]
 		
 		var chat_name = '[b]' + get_chat_name(speaker_id) + '[/b]'
-		var new_dialog =  chat_name + ':' + text
+		chat_name = color_wrap_text(chat_name + ":", speaker_id)
 		
-		new_dialog = color_wrap_text(new_dialog, speaker_id)
-		
-		all_dialog.push_back(new_dialog + "\n")
+		all_dialog.push_back({
+			"speaker_id": speaker_id,
+			"chat_name_string": chat_name,
+			"new_dialog_string": text,
+		})
 	
-	current_state = states.TALKING
+	update_state(states.TALKING)
 	self.show()
 	load_next_line()
 
@@ -47,30 +54,51 @@ func load_next_line() -> void:
 		return
 	
 	if current_dialog_index >= all_dialog.size():
-		current_state = states.INNACTIVE
-		self.hide()
+		update_state(states.INNACTIVE)
 		return
+
+	update_state(states.PAUSE)
 	
-	main_dialog_box.text = main_dialog_box.text + all_dialog[current_dialog_index]
+	var current_line = all_dialog[current_dialog_index]
+	
+	main_dialog_box.text = main_dialog_box.text + current_line.chat_name_string
+	
+	for character in current_line.new_dialog_string:
+		main_dialog_box.text = main_dialog_box.text + color_wrap_text(character, current_line.speaker_id)
+		var wait_time = 0.02
+		if character == '.':
+			wait_time = 0.15
+		
+		await get_tree().create_timer(wait_time).timeout
+	
+	main_dialog_box.text = main_dialog_box.text + '\n'
+	
 	current_dialog_index += 1
+	update_state(states.TALKING)
 
 func reset_dialog_box() -> void:
 	all_dialog = []
 	current_dialog_index = 0
 	main_dialog_box.text = ''
-	current_state = states.INNACTIVE
+	update_state(states.INNACTIVE)
 
 
 # ------ STATE MACHINE --------
-
 enum states { PAUSE, INNACTIVE, TALKING }
-var current_state = states.PAUSE
+var current_state = states.INNACTIVE
+
+func update_state(new_state: states) -> void:
+	if current_state == new_state:
+		return
+	
+	current_state = new_state
+	
+	if new_state == states.INNACTIVE:
+		self.hide()
 
 
 # ------ UTILITIES --------
-
 func get_chat_name(speaker_id: String) -> String:
-	
 	var chat_name = speaker_metadata[speaker_id].chat_name
 	return chat_name
 
